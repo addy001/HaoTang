c468f0ef0b9a02eb3c064d5c8e599a20c88faa16
miomiomiomio <408143270@qq.com>
2015-07-22 21:22:52 +0800

    添加日志管理
    添加日志管理

---
+++ GMS.Web.Admin/Areas/Account/Controllers/LogController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GMS.Account.Contract;
+using GMS.Web.Admin.Common;
+using GMS.Framework.Utility;
+using GMS.Core.Log;
+
+namespace GMS.Web.Admin.Areas.Account.Controllers
+{
+    [Permission(EnumBusinessPermission.AccountManage_Role)]
+    public class LogController : AdminControllerBase
+    {
+        //
+        // GET: /Account/Log/
+
+        public ActionResult Index(LogRequest request)
+        {
+            var result = this.AccountService.GetLogList(request);
+            return View(result);
+        }
+
+        //
+        // GET: /Account/Role/Create
+
+        public ActionResult Create()
+        {
+            var businessPermissionList = EnumHelper.GetItemValueList<EnumBusinessPermission>();
+            this.ViewBag.BusinessPermissionList = new SelectList(businessPermissionList, "Key", "Value");
+
+            var model = new AuditLog();
+            return View("Edit", model);
+        }
+
+    }
+}
